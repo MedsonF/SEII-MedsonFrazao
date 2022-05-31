5f825439e630303c66c34d5f762d42c4f947b8b1
MedsonF <medsonjosefrazao@outlook.com>
2022-05-31 20:59:15 -0300

    att

--- Semana04/py06.py
+++ Semana04/py06.py
@@ -1,72 +1,74 @@
+			# usando condicionais com comparativos
+language = 'Python'
+if language == 'Python':			
+	print('Conditional was True')
+
+
 language = 'Python'
 if language == 'Python':
-    print('Conditional was True')
-
-
-language = 'Python'
-if language == 'Python':
-    print('Language is Python')
+	print('Language is Python')
 else:
-    print('No match')
+	print('No match')
 
 
 language = 'Java'
 if language == 'Python':
-    print('Language is Python')
+	print('Language is Python')
 else:
-    print('No match')
+	print('No, is Other')
 
 
 language = 'Java'
 if language == 'Python':
-    print('Language is Python')
+	print('Language is Python')
 elif language == 'Java':
-    print('Language is Java')
+	print('Language is Java')
 else:
-    print('No match')
+	print('No, is Other')
 
 
 language = 'Java'
 if language == 'Python':
-    print('Language is Python')
+	print('Language is Python')
 elif language == 'Java':
-    print('Language is Java')
+	print('Language is Java')
 elif language == 'JavaScript':
-    print('Language is JavaScript')
+	print('Language is JavaScript')
 else:
-    print('No match')
+	print('No, is Other')
+
+
+			# condicional com mais condições
+user = 'Admin'
+login = True
+if user == 'Admin' and login:
+	print('Admin Page')
+else:
+	print('Bad Creds')
 
 
 user = 'Admin'
-logged_in = True
-if user == 'Admin' and logged_in:
-    print('Admin Page')
+login = False
+if user == 'Admin' and login:
+	print('Admin Page')
 else:
-    print('Bad Creds')
+	print('Bad Creds')
 
 
 user = 'Admin'
-logged_in = False
-if user == 'Admin' and logged_in:
-    print('Admin Page')
+login = False
+if user == 'Admin' or login:
+	print('Admin Page')
 else:
-    print('Bad Creds')
+	print('Bad Creds')
 
 
 user = 'Admin'
-logged_in = False
-if user == 'Admin' or logged_in:
-    print('Admin Page')
+login = False
+if not login:
+	print('Please Log In')
 else:
-    print('Bad Creds')
-
-
-user = 'Admin'
-logged_in = False
-if not logged_in:
-    print('Please Log In')
-else:
-    print('Welcome')
+	print('Welcome')
 
 
 a = [1,2,3]
@@ -89,43 +91,44 @@ print(id(a))
 print(id(b))
 print(id(a) == id(b))
 
+			# condicionais fixos
 condition = False
 if condition:
-    print('Evalued to True')
+	print('Evalued to True')
 else:
-    print('Evalued to False')
+	print('Evalued to False')
 
 
 condition = None
 if condition:
-    print('Evalued to True')
+	print('Evalued to True')
 else:
-    print('Evalued to False')
+	print('Evalued to False')
 
 
 condition = 10
 if condition:
-    print('Evalued to True')	
+	print('Evalued to True')	
 else:
-    print('Evalued to False')
+	print('Evalued to False')
 
-				
+							
 condition = []
 if condition:
-    print('Evalued to True')
+	print('Evalued to True')
 else:
-    print('Evalued to False')
+	print('Evalued to False')
 
 
 condition = ''
 if condition:
-    print('Evalued to True')
+	print('Evalued to True')
 else:
-    print('Evalued to False')
+	print('Evalued to False')
 
 
 condition = 'Test'
 if condition:
-    print('Evalued to True')
+	print('Evalued to True')
 else:
-    print('Evalued to False')
+	print('Evalued to False')
