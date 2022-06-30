94504ff3a58890d35225870b5ed533674c5f60c4
MedsonF <medsonjosefrazao@outlook.com>
2022-06-29 23:02:32 -0300

    Atualizações atv 8 Semana 8

--- Semana08/Calculadora/calculadora.js
+++ Semana08/Calculadora/calculadora.js
@@ -1,17 +1,12 @@
-//var valor = parseInt(0);
 var valor;
-
+var resultado;
 function botao(num){
     valor = document.calc.visor.value += num;
-    //valor += parseInt(num);
-    //document.getElementById("visor").value = valor;
 }
-
 function reseta(){
     document.calc.visor.value = '';
 }
-
 function calcula(){
     resultado = eval(valor);
-    document.calc.visor.value = resultado.toLocaleString('pt-');
+    document.calc.visor.value = resultado.toLocaleString('pt-BR');
 }
